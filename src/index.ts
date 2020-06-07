982638571a55d9ac7fe98fbbee45666c2c43f9c4
gsanta <gsanta@chemaxon.com>
2020-06-07 19:08:20 +0200

    First commit

---
+++ src/index.ts
@@ -0,0 +1,147 @@
+import * as Pixie from 'pixi.js';
+import { Sprite } from 'pixi.js';
+const PIXIE: any = Pixie;
+
+let app = new Pixie.Application({width: 256, height: 256});
+//Add the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view);
+
+const loader = new Pixie.Loader();
+
+//load an image and run the `setup` function when it's done
+loader
+    .add("assets/sprites/balloon2.png")
+    .load(setup);
+
+let cat: any;
+let state: any;
+
+function setup() {
+    cat = new Sprite(loader.resources["assets/sprites/balloon2.png"].texture);
+    cat.y = 96; 
+    cat.scale = new Pixie.Point(0.4, 0.4);
+    cat.vx = 0;
+    cat.vy = 0;
+    app.stage.addChild(cat);
+
+            //Capture the keyboard arrow keys
+    let left = keyboard("ArrowLeft"),
+        up = keyboard("ArrowUp"),
+        right = keyboard("ArrowRight"),
+        down = keyboard("ArrowDown");
+
+    //Left arrow key `press` method
+    left.press = () => {
+        //Change the cat's velocity when the key is pressed
+        cat.vx = -5;
+        cat.vy = 0;
+    };
+    
+    //Left arrow key `release` method
+    left.release = () => {
+        //If the left arrow has been released, and the right arrow isn't down,
+        //and the cat isn't moving vertically:
+        //Stop the cat
+        if (!right.isDown && cat.vy === 0) {
+        cat.vx = 0;
+        }
+    };
+
+    //Up
+    up.press = () => {
+        cat.vy = -5;
+        cat.vx = 0;
+    };
+    up.release = () => {
+        if (!down.isDown && cat.vx === 0) {
+        cat.vy = 0;
+        }
+    };
+
+    //Right
+    right.press = () => {
+        cat.vx = 5;
+        cat.vy = 0;
+    };
+    right.release = () => {
+        if (!left.isDown && cat.vy === 0) {
+        cat.vx = 0;
+        }
+    };
+
+    //Down
+    down.press = () => {
+        cat.vy = 5;
+        cat.vx = 0;
+    };
+    down.release = () => {
+        if (!up.isDown && cat.vx === 0) {
+        cat.vy = 0;
+        }
+    };
+
+    //Set the game state
+    state = play;
+
+    app.ticker.add(delta => gameLoop(delta));
+}
+
+function gameLoop(delta){
+
+    //Update the current game state:
+    state(delta);
+}
+
+function play(delta) {
+
+    //Use the cat's velocity to make it move
+    cat.x += cat.vx;
+    cat.y += cat.vy
+}
+
+function keyboard(value) {
+    let key: any = {};
+    key.value = value;
+    key.isDown = false;
+    key.isUp = true;
+    key.press = undefined;
+    key.release = undefined;
+    //The `downHandler`
+    key.downHandler = event => {
+        if (event.key === key.value) {
+        if (key.isUp && key.press) key.press();
+        key.isDown = true;
+        key.isUp = false;
+        event.preventDefault();
+        }
+    };
+
+    //The `upHandler`
+    key.upHandler = event => {
+        if (event.key === key.value) {
+        if (key.isDown && key.release) key.release();
+        key.isDown = false;
+        key.isUp = true;
+        event.preventDefault();
+        }
+    };
+
+    //Attach event listeners
+    const downListener = key.downHandler.bind(key);
+    const upListener = key.upHandler.bind(key);
+    
+    window.addEventListener(
+        "keydown", downListener, false
+    );
+    window.addEventListener(
+        "keyup", upListener, false
+    );
+    
+    // Detach event listeners
+    key.unsubscribe = () => {
+        window.removeEventListener("keydown", downListener);
+        window.removeEventListener("keyup", upListener);
+    };
+    
+    return key;
+}
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = env => {
+    return {
+        entry: './src/index.ts',
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: [/node_modules/, /src\/server/]
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader' 
+                    ]
+                },
+                {
+                    test: /\.gwm$/,
+                    use: 'raw-loader'
+                },
+                {
+                    test: /\.(png|jpg|gif)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {}
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "app.css",
+                allChunks: true
+            })
+        ],
+        resolve: {
+            extensions: [ '.tsx', '.ts', '.js', 'scss', '.css' ]
+        },
+        output: {
+            filename: 'bundle.js',            
+        },
+        context: __dirname,
+        devtool: 'eval',
+        devServer: {
+            contentBase: ['.', './test', './assets', './demo'],
+            port: 8764
+        }
+    }
+};
